4096225f715e639de6207f9a53e3269207bd7372
Eldorbek94 <eldorbtc@gmail.com>
2024-10-31 17:54:18 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("Figuring out odd or even!");
+Console.WriteLine("Input:");
+int var = Convert.ToInt32(Console.ReadLine());
+if(var%2==0) Console.WriteLine("Juft");
+else Console.WriteLine("Toq"); 
